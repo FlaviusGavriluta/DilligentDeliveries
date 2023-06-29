dea129aea56e5d776567cee8b03fda0d47b568a2
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-06-30 00:02:26 +0300

    add the constructor class

--- src/main/java/com/codecool/diligentdeliveries/Rider.java
+++ src/main/java/com/codecool/diligentdeliveries/Rider.java
@@ -3,36 +3,39 @@ package com.codecool.diligentdeliveries;
 import java.util.ArrayList;
 
 public class Rider {
-
     private int id;
     private String name;
+    private int reattemptPerAddress;
+    private int maximumReattempts;
+    private ArrayList<Parcel> parcels;
+    private Report report;
 
-    public Rider(int id, int reattemptPerAddress, int maximumReattempts){
-        //Initialize members
+    public Rider(int id, int reattemptPerAddress, int maximumReattempts) {
+        this.id = id;
+        this.reattemptPerAddress = reattemptPerAddress;
+        this.maximumReattempts = maximumReattempts;
+        this.parcels = new ArrayList<>();
+        this.report = new Report(0, maximumReattempts);
     }
 
-    public void addParcels(ArrayList<Parcel> parcels){
+    public void addParcels(ArrayList<Parcel> parcels) {
         //Implement
     }
 
-    public void startRoutine()
-    {
+    public void startRoutine() {
         //Implement
     }
 
-    private boolean reattempt(Parcel parcel)
-    {
+    private boolean reattempt(Parcel parcel) {
         //Implement
         return false;
     }
 
-    private void handleSuccessfulDelivery(Parcel parcel)
-    {
+    private void handleSuccessfulDelivery(Parcel parcel) {
         //Implement
     }
 
-    public Report getReport()
-    {
+    public Report getReport() {
         //Implement
         return null;
     }
